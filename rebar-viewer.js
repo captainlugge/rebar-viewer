0035efab8caf026695427bef93f878f4ab56c009
captainlugge <ludwigraps@web.de>
2025-09-30 20:40:59 +0200

    Update rebar-viewer.js

--- rebar-viewer.js
+++ rebar-viewer.js
@@ -1,16 +1,19 @@
-export function init($widget) {
-  console.log("✅ Wix init läuft!");
-  
-  const box = document.createElement("div");
-  box.style.width = "100%";
-  box.style.height = "100%";
-  box.style.backgroundColor = "red";
-  box.style.display = "flex";
-  box.style.alignItems = "center";
-  box.style.justifyContent = "center";
-  box.style.color = "white";
-  box.style.fontSize = "24px";
-  box.innerText = "Hallo aus GitHub Pages!";
-  
-  $widget.appendChild(box);
-}
+(function () {
+  console.log("✅ rebar-viewer.js geladen");
+
+  window.init = function ($widget) {
+    console.log("✅ Wix hat init() aufgerufen");
+
+    const box = document.createElement("div");
+    box.style.width = "100%";
+    box.style.height = "100%";
+    box.style.backgroundColor = "red";
+    box.style.color = "white";
+    box.style.display = "flex";
+    box.style.alignItems = "center";
+    box.style.justifyContent = "center";
+    box.style.fontSize = "24px";
+    box.innerText = "Hallo aus GitHub Pages!";
+    $widget.appendChild(box);
+  };
+})();
